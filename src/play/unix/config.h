6b4a3c52ae90531e0b6d6b433ae26d1965e74c1d
dave <dave>
2009-11-20 22:17:06 +0000

    This file is automatically generated

--- src/play/unix/config.h
+++ src/play/unix/config.h
@@ -1,8 +1,6 @@
-/* config.h from config.sh script Thu Nov 19 16:56:33 2009
- * Darwin grote1lt1.dhcp.lbl.gov 9.8.0 Darwin Kernel Version 9.8.0: Wed Jul 15 16:55:01 PDT 2009; root:xnu-1228.15.4~1/RELEASE_I386 i386
+/* config.h from config.sh script Fri Nov 20 14:15:44 PST 2009
+ * Linux fusion.lbl.gov 2.6.9-55.0.12.plus.c4smp #1 SMP Fri Nov 2 11:32:03 EDT 2007 x86_64 x86_64 x86_64 GNU/Linux
  */
 #define USE_GETRUSAGE
 #define USE_GETTIMEOFDAY
 #define FPU_MACOSX_INTEL
-#define USE_SELECT
-#define HAVE_SYS_SELECT_H
